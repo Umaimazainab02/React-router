cfedbf5f621263968a121e74032d477f4b7c72f5
Umaima Zainab <umaimazainab429@gmail.com>
2026-09-20 19:47:27 +0500

    Added loop practice questions

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router'
 const Navbar = () => {
     return (
         <div className='flex justify-between bg-[rgb(13,136,136)] text-2xl text-white p-7 '>
-            <h1>Umaima zainab</h1>
+            <h1 className='cursor-pointer'>Umaima zainab</h1>
             <div className='flex gap-12 underline underline-offset-4 '>
                 <Link to='/' className='hover:text-pink-300'>Home</Link>
                 <Link to='/about' className='hover:text-pink-300'>About</Link>
